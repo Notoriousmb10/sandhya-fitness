49d53f2d0f2991160dbad39719188b39eacd8081
Notoriousmb10 <yashbhingarde9@gmail.com>
2024-08-28 22:15:26 +0530

    Added Some Activity Page Content.

--- src/components/Activities/ActiviHero.jsx
+++ src/components/Activities/ActiviHero.jsx
@@ -1,14 +1,43 @@
-import React from 'react'
-
+import React from "react";
+import beachVideo from "../../assets/beachactivities.mp4";
+import trainerbg from "../../assets/trainerbg.png";
 const ActiviHero = () => {
   return (
-    <div className=''>
-      <h1 className='w-max px-24 text-3xl transform skew-x-[-20deg]  font-palanquin font-bold bg-yellow-200'>Activities?</h1>
-      <h1 className='w-max px-24 text-3xl transform skew-x-[-20deg]  font-palanquin font-bold bg-yellow-200'>No, We Dont Do Activities Here!</h1>
-      <h1 className='w-max px-24 text-3xl transform skew-x-[-20deg]  font-palanquin font-bold bg-yellow-200'>We Do?</h1>
-      <h1 className='w-max px-24 text-3xl transform skew-x-[-20deg]  font-palanquin font-bold bg-yellow-200'>We Do Magiccccccccccccccccc</h1>
+    <div
+      className="rounded-3xl md-plus:flex md-plus:justify-between md-plus:p-6 border-2 border-red-200"
+      style={{
+        backgroundImage: `url(${trainerbg})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundAttachment: "fixed",
+      }}
+    >
+      <div
+        className="flex flex-col gap-10
+      font-palanquin  md-plus:justify-evenly pb-6"
+      >
+        <h1 className="text-3xl md-plus:max-w-sm  md-plus:text-4xl mt-4 font-bold">
+          Energizing Beach <span className="text-coral-red">Workouts!</span>
+        </h1>
+        <p className="text-sm md-plus:text-lg md-plus:max-w-lg  font-montserrat ">
+          Join our refreshing sunrise workout at Gorai Beach! Start your day at
+          6:30 AM with an energizing mix of cardio and strength training, all
+          set against the stunning backdrop of the rising sun and the soothing
+          sounds of the waves. Perfect for all fitness levels!
+        </p>
+      </div>
+      <div className="p-2 md-plus:h-96 object-contain flex md-plus:justify-center ">
+        <video
+          className="rounded-3xl "
+          src={beachVideo}
+          autoPlay
+          muted
+          loop
+        ></video>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ActiviHero
+export default ActiviHero;
